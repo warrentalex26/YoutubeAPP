27252e947c8b860a62778006ab56062733bc9f13
Alexander Talavera <warrentalex26ies@gmail.com>
2019-03-12 11:22:10 -0600

    Interfaz

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { YoutubeService } from '../../services/youtube.service';
 
+declare var $: any;
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -9,12 +11,19 @@ import { YoutubeService } from '../../services/youtube.service';
 export class HomeComponent implements OnInit {
 
   videos: any [] = [];
+  videoSel: any;
 
   constructor(public youtubeService: YoutubeService) {
     this.youtubeService.getVideos().subscribe(videoYoutube => {
       this.videos = videoYoutube;
       console.log(this.videos);
     });
+  }
+
+  verVideo(video: any) {
+    this.videoSel = video;
+    $('#exampleModalLong').modal();
+
   }
 
   ngOnInit() {
